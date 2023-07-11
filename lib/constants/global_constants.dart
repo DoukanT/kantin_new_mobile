53d4fab0aa6b1393d7a679405a9a0fc0d57baef1
Yiğit Akça <yigitakca07@gmail.com>
2023-07-11 14:22:19 +0300

    'main_view' file created

---
+++ lib/constants/global_constants.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+const mainColor = Color(0xFF9E141F);
+const mainColorLight = Color.fromRGBO(190, 52, 63, 1);
+
+const secondaryColor = Color(0xFF004a85);
+const secondaryColorLight = Color(0xFF2c6f9d);
---
+++ lib/views/main_view.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:kantin_new_mobile/constants/global_constants.dart';
+import 'package:badges/badges.dart' as badges;
+import 'package:kantin_new_mobile/views/menu_view.dart';
+
+class MainView extends StatefulWidget {
+  const MainView({super.key});
+
+  @override
+  State<MainView> createState() => _MainViewState();
+}
+
+int viewIndex = 0;
+const views = [
+  MenuView(), // Menu
+  MenuView(), // Past Orders
+  MenuView(), // Profile
+  MenuView(), // Cart
+];
+
+class _MainViewState extends State<MainView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Kantin"),
+        centerTitle: true,
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 8),
+            child: IconButton(
+              icon: const badges.Badge(
+                badgeContent: Text('3'),
+                badgeStyle: badges.BadgeStyle(
+                  badgeColor: secondaryColorLight,
+                ),
+                child: Icon(
+                  Icons.shopping_cart,
+                  color: Colors.white,
+                ),
+              ),
+              onPressed: () {
+                setState(() {
+                  viewIndex = 3;
+                });
+              },
+            ),
+          ),
+        ],
+        backgroundColor: mainColor,
+      ),
+      bottomNavigationBar: BottomAppBar(
+        color: mainColor,
+        shape: const CircularNotchedRectangle(),
+        notchMargin: 5,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            IconButton(
+              onPressed: () => {
+                setState(() {
+                  viewIndex = 1;
+                })
+              },
+              icon: const Icon(Icons.receipt_long),
+              color: Colors.white,
+            ),
+            IconButton(
+              onPressed: () => {
+                setState(() {
+                  viewIndex = 2;
+                })
+              },
+              icon: const Icon(Icons.person),
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => {
+          setState(() {
+            viewIndex = 0;
+          })
+        },
+        backgroundColor: secondaryColor,
+        child: const Icon(Icons.dining),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      body: views[viewIndex],
+    );
+  }
+}
